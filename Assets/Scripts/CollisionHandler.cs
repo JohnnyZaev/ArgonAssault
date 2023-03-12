09a72edb469424fd136b72f97dee71c445ba448f
gvarys <johnnyzaev@gmail.com>
2023-03-12 17:11:05 +0300

    Particles now triggers then we crush

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private float loadDelay = 2f;
+    [SerializeField] private ParticleSystem crushVFX;
     
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +14,9 @@ public class CollisionHandler : MonoBehaviour
     private void StartCrushSequence()
     {
         GetComponent<MovementController>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        crushVFX.Play();
         Invoke(nameof(ReloadLevel), loadDelay);
     }
 
