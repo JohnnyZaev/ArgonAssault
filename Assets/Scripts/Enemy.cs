8953bff94a5ef56fdd4d680b51aac1c4173f7663
gvarys <johnnyzaev@gmail.com>
2023-03-13 19:52:30 +0300

    now enemy triggers particles too

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject crushVFX;
     private void OnParticleCollision(GameObject other)
     {
+        var transform1 = transform;
+        var VFX = Instantiate(crushVFX, transform1.position, transform1.rotation);
+        VFX.GetComponent<ParticleSystem>().Play();
         Destroy(gameObject);
     }
 }
